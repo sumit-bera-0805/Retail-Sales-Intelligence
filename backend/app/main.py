1f5bcdf6c0b9097acb5fb4aad32aeb10b96833e1
sumit-bera-0805 <berasumit052@gmail.com>
2026-07-03 15:37:38 +0530

    Added Professional Landing Page

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-# Import Routers
 from backend.app.api.health import router as health_router
 from backend.app.api.dashboard import router as dashboard_router
 from backend.app.api.sales import router as sales_router
@@ -8,14 +9,14 @@ from backend.app.api.customers import router as customers_router
 from backend.app.api.products import router as products_router
 from backend.app.api.regions import router as regions_router
 
-# Create FastAPI App
 app = FastAPI(
     title="Retail Sales Intelligence API",
-    description="End-to-End Retail Sales Intelligence Platform using FastAPI, PostgreSQL, SQL and Power BI",
+    description="Enterprise Retail Analytics Platform",
     version="1.0.0"
 )
 
-# Register Routers
+templates = Jinja2Templates(directory="backend/app/templates")
+
 app.include_router(health_router)
 app.include_router(dashboard_router)
 app.include_router(sales_router)
@@ -23,11 +24,12 @@ app.include_router(customers_router)
 app.include_router(products_router)
 app.include_router(regions_router)
 
-# Home Route
-@app.get("/", tags=["Home"])
-def home():
-    return {
-        "message": "Retail Sales Intelligence API is Running 🚀",
-        "version": "1.0.0",
-        "documentation": "http://127.0.0.1:8000/docs"
-    }
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request
+        }
+    )
